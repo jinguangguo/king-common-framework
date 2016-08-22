f25a98e08263a634e1441d388c71f2e6da4e34dd
jinguangguo <jinguangguo@new4g.cn>
2016-08-22 19:04:45 +0800

    fixbug

--- app/js/index.js
+++ app/js/index.js
@@ -11,13 +11,13 @@ import subEllipsis from '../../king-util/src/subEllipsis';
 import plus from '../../king-util/src/plus';
 import minus from '../../king-util/src/minus';
 import multiply from '../../king-util/src/multiply';
-import divide from '../../king-util/src/divide';
+import {xxx} from '../../king-util/src/divide';
 
 console.log(subEllipsis('还不错哦还不错哦', 10));
 
 console.log(plus(0.1, 0.2));
 console.log(minus(0.32, 0.11));
 console.log(multiply(0.1, 0.2));
-console.log(divide(0.1, 0.2));
+console.log(xxx(0.1, 0.2));
 
 
